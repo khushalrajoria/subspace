75c12a0c94ce57916e3d88caead60eae5a61a2e7
Khushal rajoria <rajoriakhushal@gmail.com>
2024-08-08 21:11:57 +0530

    removed unnecessary files

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
-
 import '../services/bloc_services.dart';
 
 // Events
--- lib/blocList_view.dart
+++ lib/blocList_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'bloc/bloc.dart';
-import 'block_detailView.dart';
+import 'bloc_detailView.dart';
 
 class BlogListView extends StatelessWidget {
   @override
