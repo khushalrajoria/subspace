6a9dea77d307e65e7c87708ea2f63e44d3ecb3b8
Khushal rajoria <rajoriakhushal@gmail.com>
2024-08-08 21:52:55 +0530

    minor changes

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -2,7 +2,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import '../services/bloc_services.dart';
 
-// Events
 abstract class BlogEvent extends Equatable {
   @override
   List<Object> get props => [];
@@ -19,8 +18,6 @@ class ToggleFavorite extends BlogEvent {
   List<Object> get props => [blog];
 }
 
-
-// States
 abstract class BlogState extends Equatable {
   @override
   List<Object> get props => [];
@@ -39,12 +36,11 @@ class BlogLoaded extends BlogState {
 
 class BlogError extends BlogState {}
 
-// Bloc
+
 class BlogBloc extends Bloc<BlogEvent, BlogState> {
   final BlogService blogService;
 
   BlogBloc(this.blogService) : super(BlogLoading()) {
-    // Registering the event handler for LoadBlogs
     on<LoadBlogs>((event, emit) async {
       emit(BlogLoading());
       try {
@@ -55,13 +51,12 @@ class BlogBloc extends Bloc<BlogEvent, BlogState> {
       }
     });
 
-    // Registering the event handler for ToggleFavorite
    on<ToggleFavorite>((event, emit) {
   if (state is BlogLoaded) {
     final loadedState = state as BlogLoaded;
     final updatedBlogs = loadedState.blogs.map((blog) {
       if (blog == event.blog) {
-        blog.toggleFavorite(); // Make sure toggleFavorite is properly modifying the isFavorite field
+        blog.toggleFavorite(); 
       }
       return blog;
     }).toList();
